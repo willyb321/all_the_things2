b9f60f360d59536285d6507e8966e90873422b2d
willyb321 <wbwilliam7@gmail.com>
2017-05-23 16:06:02 +1000

    various bits

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -4,19 +4,7 @@
 'use strict';
 
 import $ from 'jquery';
-import Link from '../_modules/link/link';
 
 $(() => {
-  new Link(); // Activate Link modules logic
-//   $.ajax({url: 'https://api.github.com/users/willyb321/repos?type=all'})
-//   .then(data => {
-// 	console.log(data);
-// 	const ul = document.createElement('ul');
-// 	for (let i in data) {
-// 		const li = document.createElement('li');
-// 		li.innerText = data[i].name + ' - ' + data[i].description;
-// 		ul.appendChild(li);
-// 	}
-// 	document.getElementById('repos').appendChild(ul);
-//   });
+
 });
