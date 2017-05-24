6aa69ae6652799f262054a9e780f1c45bedce8b7
willyb321 <wbwilliam7@gmail.com>
2017-05-24 15:00:42 +1000

    only show owner and collaborator repos

--- update-repos.sh
+++ update-repos.sh
@@ -1,1 +1,2 @@
-curl -o src/_data/repos.json -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/user/repos?per_page=100&visibility=public
+#!/usr/bin/env bash
+curl -o src/_data/repos.json -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/user/repos?per_page=100&visibility=public&affiliation=owner,collaborator
