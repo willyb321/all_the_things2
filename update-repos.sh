545c1dd162213e3acef4177078af3d3bc48aadc3
willyb321 <wbwilliam7@gmail.com>
2017-05-24 12:50:40 +1000

    make sure description isnt null

--- update-repos.sh
+++ update-repos.sh
@@ -1,1 +1,1 @@
-curl -o src/_data/repos.json -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/user/repos?per_page=100&visibility=public 
+curl -o src/_data/repos.json -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/user/repos?per_page=100&visibility=public
